95815b672b529bd2747e82be71f434687f6af9dc
sarahllg <a.sarah315@gmail.com>
2025-12-15 09:55:15 +0100

    Add Usine struct and related function declarations

--- src/usine.h
+++ src/usine.h
@@ -1,1 +1,18 @@
+#ifndef USINE_H
+#define USINE_H
+
+typedef struct {
+    char id[50];      // Id de l'usine
+    double max;       // Capacité max
+    double src;       // Volume capté
+    double real;      // Volume réellement traité
+} Usine;
+
+
+Usine* creer_usine(const char* id, double max, double src, double real);
+
+
+void afficher_usine(const Usine* u);
+
+#endif
 
