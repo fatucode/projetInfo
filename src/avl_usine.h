f2debf67d16e6a0c7965497b0e3a860bb0f8d2bf
sarahllg <a.sarah315@gmail.com>
2025-12-15 16:11:56 +0100

    Add AVL tree structure and function declarations

--- src/avl_usine.h
+++ src/avl_usine.h
@@ -1,1 +1,23 @@
+#ifndef AVL_USINE_H
+#define AVL_USINE_H
+#include "usine.h"
+
+typedef struct AvlUsine {
+    Usine* usine;              // Donnée stockée
+    int hauteur;               // Hauteur du noeud
+    struct AvlUsine* gauche;   // Fils gauche
+    struct AvlUsine* droite;   // Fils droit
+} AvlUsine;
+
+AvlUsine* creer_noeud_usine(Usine* u);
+
+AvlUsine* inserer_usine(AvlUsine* racine, Usine* u);
+
+Usine* chercher_usine(AvlUsine* racine, const char* id);
+
+void parcours_inverse(AvlUsine* racine, FILE* out, int mode);
+
+void liberer_avl_usine(AvlUsine* racine);
+
+#endif
 
