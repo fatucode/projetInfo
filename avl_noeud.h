c4ed5cb7946458accecf9714d193db5ca6d00b82
fatucode <wannjalloh@gmail.com>
2025-12-15 11:40:41 +0100

    Define AVLNode structure and related functions

--- avl_noeud.h
+++ avl_noeud.h
@@ -1,1 +1,29 @@
+#ifndef AVL_H
+#define AVL_H
+
+#include "reseau.h"
+#define MAX_ID 50
+
+typedef struct AVLNode {
+    char id[MAX_ID];
+    Noeud* ptr_noeud;
+    struct AVLNode* left;
+    struct AVLNode* right;
+    int height;
+} AVLNode;
+
+// --- Fonctions générales ---
+int height(AVLNode* n);
+int max(int a, int b);
+
+// --- Recherche / insertion ---
+AVLNode* avl_search(AVLNode* root, const char* id);
+AVLNode* avl_insert(AVLNode* root, const char* id, Noeud* ptr_noeud);
+
+// --- Rotations ---
+AVLNode* rotate_left(AVLNode* x);
+AVLNode* rotate_right(AVLNode* y);
+int get_balance(AVLNode* n);
+
+#endif
 
