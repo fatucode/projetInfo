2c527a81ac2b680e0c83745330d5045d44842f24
sarahllg <a.sarah315@gmail.com>
2025-12-15 16:35:28 +0100

    Update histogramme.h

--- src/histogramme.h
+++ src/histogramme.h
@@ -1,1 +1,16 @@
+#ifndef HISTOGRAMME_H
+#define HISTOGRAMME_H
+
+#include "avl_usine.h"
+
+// Modes d'histogramme
+#define HISTO_MAX  0
+#define HISTO_SRC  1
+#define HISTO_REAL 2
+#define HISTO_ALL  3
+
+// Générer le CSV de l'histogramme
+int generer_histogramme(AvlUsine* racine, const char* nom_fichier, int mode);
+
+#endif
 
