7f9f65c544dfec38eed98bc52484b68a4c5301e7
fatucode <wannjalloh@gmail.com>
2025-12-16 12:23:55 +0100

    Add CSV reader header with processing function

---
+++ csv_reader.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef CSV_READER_H 
+#define CSV_READER_H
+
+
+typedef void (*csv_line_cb)(const char* col1, const char* amont, const char* aval,
+                            const char* volume, const char* fuite, void* user_data);
+int csv_process_file(const char* filename, csv_line_cb cb, void* user_data);
+
+#endif
