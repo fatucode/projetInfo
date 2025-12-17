fffbbceb34822fdb8bbb12ed9e6db001cb4e3140
fatucode <wannjalloh@gmail.com>
2025-12-17 14:31:30 +0100

    Modify header guards and MAX_ID in reseau.h
    Updated header guards and increased MAX_ID value.

--- reseau.h
+++ reseau.h
@@ -1,8 +1,11 @@
-#ifndef RESEAU_H
-#define RESEAU_H
+#ifndef RESEAAU_H
+#define RESEAAU_H
 
-#define MAX_ID 50
+#define MAX_ID 100
 
+struct Noeud;  
+struct AVLNode;
+typedef struct AVLNode AVLNode;
 typedef enum {
     SOURCE,
     USINE,
@@ -12,22 +15,26 @@ typedef enum {
     USAGER
 } TypeNoeud;
 
+typedef struct Noeud Noeud;
+
 typedef struct ChildNode {
     struct Noeud* enfant;       // pointeur vers le vrai nœud enfant
     struct ChildNode* suivant;  // maillon suivant
 } ChildNode;
 
-typedef struct Noeud {
+struct Noeud {
     char id[MAX_ID];
     TypeNoeud type;
     double volume;
     double fuite;
 
     ChildNode* enfants;         // liste d'enfants
-} Noeud;
+} ;
+
 
 // Prototypes
 Noeud* creer_noeud(const char* id, TypeNoeud type);
 void ajouter_enfant(Noeud* parent, Noeud* enfant);
-
+AVLNode* charger_reseau(const char* filename);
+TypeNoeud detecter_type(const char* id, int role);
 #endif
