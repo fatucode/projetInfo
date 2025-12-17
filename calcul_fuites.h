b2bf8d4c316e1ec5487d3a1430594bb988f07ed8
mariacode237 <mariatazi23@gmail.com>
2025-12-17 16:19:00 +0100

    Update calcul_fuites.h

--- calcul_fuites.h
+++ calcul_fuites.h
@@ -6,6 +6,11 @@
 
 // Lance le calcul à partir d’une usine racine 
 double calculer_fuites(Noeud* racine); 
+//Ecrit les fuites dans le fichier.dat
+int ecrire_fuite_dat(const char* filename,
+                     const char* id_usine,
+                     double volume_perdu);
+
 
 // Calcul récursif interne 
 double parcourir(Noeud* n, double volume_entree);
