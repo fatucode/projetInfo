6dd35385652b131b6183b174dc8059f6cc895e36
fatucode <wannjalloh@gmail.com>
2025-12-18 17:17:42 +0100

    Refactor fuite calculation functions in header

--- calcul_fuites.h
+++ calcul_fuites.h
@@ -3,15 +3,12 @@
 #define CALCUL_FUITES_H 
 #include <stdio.h>
 #include "reseaau.h" 
-
-// Lance le calcul à partir d’une usine racine 
-double calculer_fuites(Noeud* racine); 
-//Ecrit les fuites dans le fichier.dat
-int ecrire_fuite_dat(const char* filename,
-                     const char* id_usine,
-                     double volume_perdu);
-
+#include "avl.h"
 
 // Calcul récursif interne 
 double parcourir(Noeud* n, double volume_entree);
  #endif
+// Lance le calcul à partir d’une usine racine 
+double calculer_fuites_usine(AVLNode* racine, const char* id_usine);
+void sauvegarder_fuites(const char* id_usine, double pertes);
+
