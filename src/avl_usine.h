dfa95a205c590942f89d641bd6520e54351fc3c4
fatucode <wannjalloh@gmail.com>
2025-12-19 17:17:52 +0100

    Add AVL tree utility functions in avl_usine.h

--- src/avl_usine.h
+++ src/avl_usine.h
@@ -10,12 +10,16 @@ typedef struct AvlUsine {
 } AvlUsine;
 
 AvlUsine* inserer_usine(AvlUsine* racine, Usine* u);
-
+int hauteur_usine(AvlUsine* n);
 Usine* chercher_usine(AvlUsine* racine, const char* id);
-
+int max(int a, int b);
 void parcours_inverse(AvlUsine* racine, FILE* out, int mode);
-
+AvlUsine* creer_noeud_usine(Usine* u);
 void liberer_avl_usine(AvlUsine* racine);
-
+int comparer_id(const char* a, const char* b);
+int facteur_equilibre_usine(AvlUsine* n);
+AvlUsine* rotation_droite_usine(AvlUsine* y);
+AvlUsine* rotation_gauche_usine(AvlUsine* x);
+AvlUsine* inserer_usine(AvlUsine* racine, Usine* u);
+void liberer_avl_usine(AvlUsine* racine);
 #endif
-
